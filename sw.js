4e9eac67ac87cdd2eecd4340d2d74fc3938a2938
unknown <tim58841@gmail.com>
2026-08-15 17:29:16 +0200

    Polish mobile UX rating and exit flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='last-patrol-v4';
+const CACHE='last-patrol-v5';
 const ASSETS=['./','./index.html','./styles.css','./src/game.js','./manifest.webmanifest','./assets/icon.svg','./assets/icon-192.png','./assets/icon-512.png','./assets/north-kivu-terrain.webp'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
